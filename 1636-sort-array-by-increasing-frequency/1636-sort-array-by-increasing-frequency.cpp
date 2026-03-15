b6101f20ff380e553d5eb91484108ff48d1e001b
Rishabh <149367535+rishabh-ydv23@users.noreply.github.com>
2026-03-15 22:46:48 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
@@ -1,17 +1,36 @@
 class Solution {
 public:
+
+    struct cmp{
+        bool operator()(pair<int,int>&a,pair<int,int>&b){
+            if(a.first==b.first)
+                return a.second<b.second;       //larger value forst
+
+            return a.first > b.first;       //smaller freq first;
+        }
+    };
     vector<int> frequencySort(vector<int>& nums) {
         unordered_map<int,int>freq;
         for(int num:nums){
             freq[num]++;
         }
 
-        sort(nums.begin(),nums.end(),[&](int a,int b){
-            if(freq[a]==freq[b]){
-                return a>b;
+        priority_queue<pair<int,int>,vector<pair<int,int>>,cmp>pq;
+
+        for(auto it:freq){
+            pq.push({it.second,it.first});
+        }
+
+        vector<int>ans;
+        while(!pq.empty()){
+            int f=pq.top().first;
+            int ele=pq.top().second;
+            pq.pop();
+
+            for(int i=0;i<f;i++){
+                ans.push_back(ele);
             }
-            return freq[a]<freq[b];
-        });
-        return nums;
+        }
+        return ans;
     }
 };
