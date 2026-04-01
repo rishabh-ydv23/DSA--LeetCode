1b5f92bb6bd405e093a48e3880a33c870984c753
Rishabh <149367535+rishabh-ydv23@users.noreply.github.com>
2026-04-01 16:59:10 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -1,23 +1,47 @@
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
-        int n=fruits.size();
-        unordered_map<int,int>mp;
-        int i=0,r=0;
-        int maxCount=0;
+        // int n=fruits.size();
+        // unordered_map<int,int>mp;
+        // int i=0,r=0;
+        // int maxCount=0;
 
-        while(r<n){
-            mp[fruits[r]]++;
-            while(mp.size()>2){
+        // while(r<n){
+        //     mp[fruits[r]]++;
+        //     while(mp.size()>2){
+        //         mp[fruits[i]]--;
+        //         if(mp[fruits[i]]==0){
+        //             mp.erase(fruits[i]);
+        //         }
+        //         i++;
+        //     }
+        //     maxCount=max(maxCount,r-i+1);
+        //     r++;
+        // }
+        // return maxCount;
+
+
+
+        int n = fruits.size();
+        unordered_map<int,int> mp;
+        
+        int i = 0;
+        int maxCount = 0;
+
+        for(int r = 0; r < n; r++){
+            mp[fruits[r]]++;   // correct
+
+            while(mp.size() > 2){   // fix
                 mp[fruits[i]]--;
-                if(mp[fruits[i]]==0){
-                    mp.erase(fruits[i]);
+                if(mp[fruits[i]] == 0){
+                    mp.erase(fruits[i]);   // fix
                 }
                 i++;
             }
-            maxCount=max(maxCount,r-i+1);
-            r++;
+
+            maxCount = max(maxCount, r - i + 1);
         }
+
         return maxCount;
     }
 };
