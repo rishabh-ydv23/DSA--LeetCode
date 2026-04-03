ceab1d70d22e5dc37ff40cb29c9bd21c65dff746
Rishabh <149367535+rishabh-ydv23@users.noreply.github.com>
2026-04-03 22:45:00 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -1,23 +1,28 @@
-
-// Hum prefix me oddCount track karte hain aur har step pe check karte hain ki kya past me aisa prefix exist karta hai jiska oddCount = current oddCount - k ho — agar haan, to utne subarrays mil jate hain jinke andar exactly k odd numbers hain.
-
 class Solution {
 public:
     int numberOfSubarrays(vector<int>& nums, int k) {
-        unordered_map<int,int>mp;
         int n=nums.size();
         int oddCount=0;
+        int prevCount=0;
         int result=0;
 
-        mp[oddCount]=1;
+        int i=0,j=0;
 
-        for(int i=0;i<n;i++){
-            oddCount+=(nums[i]%2);
-
-            if(mp.count(oddCount-k)){
-                result+=mp[oddCount-k];
+        while(j<n){
+            if(nums[j]%2!=0){
+                oddCount++;
+                prevCount=0;
             }
-            mp[oddCount]++;
+
+            while(oddCount==k){
+                prevCount++;
+                if(nums[i]%2==1){
+                    oddCount--;
+                }
+                i++;
+            }
+            result+=prevCount;
+            j++;
         }
         return result;
     }
