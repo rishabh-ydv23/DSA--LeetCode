844edfe34bc6055c32d3785013c8c380bb1b0d5f
Rishabh <149367535+rishabh-ydv23@users.noreply.github.com>
2026-04-05 12:33:48 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0415-add-strings/0415-add-strings.cpp
+++ 0415-add-strings/0415-add-strings.cpp
@@ -25,3 +25,5 @@ public:
         return res;
     }
 };
+
+//This problem is solved by simulating the manual addition of two numbers. We start from the last digits of both strings using two pointers and maintain a carry variable. In each step, we add the digits of both strings along with the carry, where characters are converted to integers using 'digit - '0'. The result digit (sum % 10) is converted back to a character and appended to the result string, and the carry is updated as sum / 10. This process continues until all digits are processed and no carry remains. Since the result is built in reverse order, we finally reverse the string to get the correct answer. This approach works efficiently for very large numbers without converting them into integers.
