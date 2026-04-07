2ef00470c03bde3fba48c186fa22bd5b05da44a0
Rishabh <149367535+rishabh-ydv23@users.noreply.github.com>
2026-04-07 22:07:48 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0067-add-binary/0067-add-binary.cpp
+++ 0067-add-binary/0067-add-binary.cpp
@@ -1,3 +1,33 @@
+// class Solution {
+// public:
+//     string addBinary(string a, string b) {
+//         int m=a.length()-1;
+//         int n=b.length()-1;
+
+//         string result="";
+//         int sum=0;
+//         int carry=0;
+
+//         while(m>=0 || n>=0 || carry){
+//             sum=carry;
+//             if(m>=0){
+//                 sum+=a[m]-'0';
+//                 m--;
+//             }
+//             if(n>=0){
+//                 sum+=b[n]-'0';
+//                 n--;
+//             }
+
+//             result+=char((sum%2)+'0');
+//             carry=sum/2;
+//         }
+//         reverse(result.begin(),result.end());
+//         return result;
+//     }
+// };
+
+
 class Solution {
 public:
     string addBinary(string a, string b) {
@@ -8,7 +38,7 @@ public:
         int sum=0;
         int carry=0;
 
-        while(m>=0 || n>=0 || carry){
+        while(m>=0 || n>=0 ){
             sum=carry;
             if(m>=0){
                 sum+=a[m]-'0';
@@ -18,11 +48,15 @@ public:
                 sum+=b[n]-'0';
                 n--;
             }
-
-            result+=char((sum%2)+'0');
-            carry=sum/2;
+            result.push_back(sum%2==0?'0':'1');
+            carry=(sum>1?1:0);
+        }
+        if(carry){
+            result.push_back('1');
         }
         reverse(result.begin(),result.end());
         return result;
     }
 };
+
+
