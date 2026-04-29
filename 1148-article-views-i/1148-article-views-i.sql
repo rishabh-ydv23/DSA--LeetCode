2e9f5bf4eeb7bc10560cff28d08884c8c571032a
Rishabh <149367535+rishabh-ydv23@users.noreply.github.com>
2026-04-29 22:05:58 +0530

    Time: 447 ms (22.52%), Space: 0B (100%) - LeetHub

---
+++ 1148-article-views-i/1148-article-views-i.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+Select DISTINCT author_id as id from views where author_id = viewer_id order by author_id ASC;
+
+
+-- 🔥 Final Understanding
+-- Requirement	        SQL
+-- Ascending	        ORDER BY col ASC
+-- Default ascending	ORDER BY col
+-- Descending	        ORDER BY col DESC
+
+-- DISTINCT
