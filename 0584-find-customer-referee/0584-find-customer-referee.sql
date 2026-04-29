5c034d602f2e48b463b548ca22096c34023b3ee2
Rishabh <149367535+rishabh-ydv23@users.noreply.github.com>
2026-04-29 21:05:33 +0530

    Time: 535 ms (15.68%), Space: 0B (100%) - LeetHub

--- 0584-find-customer-referee/0584-find-customer-referee.sql
+++ 0584-find-customer-referee/0584-find-customer-referee.sql
@@ -1,2 +1,19 @@
 # Write your MySQL query statement below
 Select name from Customer where referee_id !='2' OR referee_id IS NULL;
+
+-- IS ka use special values ke saath hota hai, mainly:
+-- NULL
+-- TRUE / FALSE (kuch DBs me)
+-- 👉 Sabse important: IS NULL
+-- SQL me NULL ka matlab hota hai unknown / missing value
+-- referee_id = NULL
+-- referee_id == NULL  (==)naam ke cheez hoti he nhi hai
+
+
+-- | Situation    | Use this        |
+-- | ------------ | --------------- |
+-- | Normal value | `=` or `!=`     |
+-- | NULL check   | `IS` / `IS NOT` |
+
+-- = → compare karta hai values ko
+-- IS → check karta hai existence / absence (NULL)
