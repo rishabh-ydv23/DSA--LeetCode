5a6fe95a73777e5036c925433aba58e8f12d256a
Rishabh <149367535+rishabh-ydv23@users.noreply.github.com>
2026-05-03 15:03:02 +0530

    Time: 643 ms (22.3%), Space: 0B (100%) - LeetHub

--- 1683-invalid-tweets/1683-invalid-tweets.sql
+++ 1683-invalid-tweets/1683-invalid-tweets.sql
@@ -1,2 +1,23 @@
 # Write your MySQL query statement below
 Select tweet_id from Tweets where CHAR_LENGTH(content)>15;
+-- Select tweet_id from Tweets where LENGTH(content)>15;
+
+
+-- | Function        | DB Used In        | Counts     | Special Behavior           |
+-- | --------------- | ----------------- | ---------- | -------------------------- |
+-- | `LENGTH()`      | MySQL, PostgreSQL | Bytes      | Unicode chars may count >1 |
+-- | `CHAR_LENGTH()` | MySQL, PostgreSQL | Characters | Accurate text length       |
+-- | `LEN()`         | SQL Server        | Characters | Ignores trailing spaces    |
+
+
+-- LENGTH()
+--     👉 Used in: MySQL, PostgreSQL, Oracle
+--     Returns number of bytes, not characters
+--     Important when dealing with Unicode / multi-byte characters
+
+
+-- 2. CHAR_LENGTH()
+-- 👉 Used in: MySQL, PostgreSQL
+-- Returns number of characters, not bytes
+-- Safe for Unicode strings
+-- This is usually what you want when counting actual text length
