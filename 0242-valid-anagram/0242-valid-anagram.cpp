5f2b8223a933c65a0f9f977568bd03f6a5307a45
Rishabh <149367535+rishabh-ydv23@users.noreply.github.com>
2026-06-25 15:15:02 +0530

    Time: 0 ms (100%), Space: 9.7 MB (75.84%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length()!=t.length()) return false;
+
+        vector<int>freq(26,0);
+        for(char ch:s){
+            freq[ch-'a']++;
+        }
+
+        for(char ch:t){
+            freq[ch-'a']--;
+        }
+
+        for(char ch:s){
+            if(freq[ch-'a']!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
