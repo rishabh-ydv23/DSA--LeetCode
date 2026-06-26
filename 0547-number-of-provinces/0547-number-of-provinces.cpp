72aba0605c703bcbf3b92ae073673672c7773665
Rishabh <149367535+rishabh-ydv23@users.noreply.github.com>
2026-06-26 21:51:41 +0530

    Time: 3 ms (37.57%), Space: 19.3 MB (83.57%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -26,3 +26,43 @@ public:
         return province;
     }
 };
+
+
+    // void dfs(unordered_map<int, vector<int>>& adj, int u, vector<bool>& visited) {
+    //     visited[u] = true;
+
+    //     // Visit all neighbours
+    //     for(int &v : adj[u]) {
+    //         if(!visited[v]) {
+    //             dfs(adj, v, visited);
+    //         }
+    //     }
+    // }
+
+    // int findCircleNum(vector<vector<int>>& isConnected) {
+    //     int n = isConnected.size();
+
+    //     // Create adjacency list from adjacency matrix
+    //     unordered_map<int, vector<int>> adj;
+
+    //     for(int i = 0; i < n; i++) {
+    //         for(int j = 0; j < n; j++) {
+    //             if(isConnected[i][j] == 1) {
+    //                 adj[i].push_back(j);
+    //                 adj[j].push_back(i);
+    //             }
+    //         }
+    //     }
+
+    //     vector<bool> visited(n, false);
+    //     int count = 0;
+
+    //     // Count connected components (provinces)
+    //     for(int i = 0; i < n; i++) {
+    //         if(!visited[i]) {
+    //             count++;
+    //             dfs(adj, i, visited);
+    //         }
+    //     }
+
+    //     return count;
